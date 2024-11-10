bbe8df1a05f38306b6e187c504596ab69e71fa68
Hein Min Thant <heinminthant259@gmail.com>
2024-11-10 16:04:23 +0630

    Implements Navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -10,15 +10,15 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("dark");
 
   const handleThemeChange = () => {
     if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.add("light");
     }
   };
 
