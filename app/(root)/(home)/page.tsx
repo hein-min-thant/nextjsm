3745d3848a4b5928ca445691f1481c1161662104
Hein Min Thant <heinminthant259@gmail.com>
2024-11-15 11:58:28 +0630

    Implements filters and no question placeholder

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,40 @@
+import Filter from "@/app/components/shared/Filter";
+import HomeFilters from "@/app/components/shared/home/HomeFilters";
+import NoResult from "@/app/components/shared/NoResult";
 import LocalSearchbar from "@/app/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
+
+const questions = [
+  // {
+  //   _id: 1,
+  //   title: "Cascading Deletes in SQLAlchemy?",
+  //   tags: [
+  //     { _id: 1, name: "python" },
+  //     { _id: 2, name: "sql" },
+  //   ],
+  //   authors: "Jhon Doe",
+  //   upvote: 10,
+  //   views: 100,
+  //   answers: 2,
+  //   createdAt: "2021-09-01T1:00:00.000Z",
+  // },
+  // {
+  //   _id: 2,
+  //   title: "How to center a div ",
+  //   tags: [
+  //     { _id: 1, name: "css" },
+  //     { _id: 2, name: "html" },
+  //   ],
+  //   authors: "Jhon Doe",
+  //   upvote: 11,
+  //   views: 167,
+  //   answers: 3,
+  //   createdAt: "2021-09-01T1:00:00.000Z",
+  // },
+];
 
 const Home = () => {
   return (
@@ -9,7 +42,7 @@ const Home = () => {
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+        <Link href="/ask-questions" className="flex justify-end max-sm:w-full">
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
             Ask a Question
           </Button>
@@ -24,7 +57,20 @@ const Home = () => {
           placeholder="Search for questions"
           otherClasses="flex-1"
         />
-        Filter
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+      <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => "Questioncard")
+        ) : (
+          <NoResult title="There's no question to show" />
+        )}
       </div>
     </>
   );
