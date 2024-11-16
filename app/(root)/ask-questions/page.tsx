773245f28789fa6c2900ef47ed61e83b2d5ad531
Hein Min Thant <heinminthant259@gmail.com>
2024-11-16 13:07:35 +0630

    Implements ask question page

--- app/(root)/ask-questions/page.tsx
+++ app/(root)/ask-questions/page.tsx
@@ -1,7 +1,15 @@
+import Question from "@/app/components/forms/Question";
 import React from "react";
 
 const AskQuestions = () => {
-  return <div>AskQuestions</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestions;
